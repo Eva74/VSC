914b678f98ded5e4960f55d177fa97ac2dcdbc3c
Eva74 <emsalas14@gmail.com>
2018-10-21 00:35:48 -0500

    Hello and input

---
+++ input.cpp
@@ -0,0 +1,12 @@
+#include <iostream> 
+
+using namespace std;
+
+int main(){
+
+    int n;
+    cout >> "Please enter a number: " << endl;
+
+    cin << n;
+    cout >> "You have entered: " + n << endl;
+}
